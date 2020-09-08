00569353061b2ff24476a5b5f0887c2b6887448f
dongcntt77 <36391847+dongcntt77@users.noreply.github.com>
2020-09-08 09:21:53 +0700

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,22 +5,29 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
   public loginForm: FormGroup;
-  constructor(private router: Router,) { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
-      'username': new FormControl('', Validators.required),
-      'password': new FormControl('', [Validators.required,Validators.minLength(6)]),
-      'remember': new FormControl(false, []),
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      remember: new FormControl(false, []),
     });
   }
   onSubmit(value: any) {
-    let xx =  value;
+    let xx = value;
     debugger;
-   // this.router.navigate(['']);
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+    this.router.navigate(['']);
   }
 }
